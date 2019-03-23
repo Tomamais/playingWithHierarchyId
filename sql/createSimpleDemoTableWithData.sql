b744b726f744ec2e81c21d9bcd830695f6773c5e
Tomas Ormeno <tomamais@Tomass-MacBook-Air.local>
2019-03-23 16:04:41 -0400

    Initial commit.

---
+++ sql/createSimpleDemoTableWithData.sql
@@ -0,0 +1,19 @@
+CREATE TABLE SimpleDemo  
+(Level hierarchyid NOT NULL,  
+Location nvarchar(30) NOT NULL,  
+LocationType nvarchar(9) NULL);  
+
+INSERT SimpleDemo  
+VALUES   
+('/1/', 'Europe', 'Continent'),  
+('/2/', 'South America', 'Continent'),  
+('/1/1/', 'France', 'Country'),  
+('/1/1/1/', 'Paris', 'City'),  
+('/1/2/1/', 'Madrid', 'City'),  
+('/1/2/', 'Spain', 'Country'),  
+('/3/', 'Antarctica', 'Continent'),  
+('/2/1/', 'Brazil', 'Country'),  
+('/2/1/1/', 'Brasilia', 'City'),  
+('/2/1/2/', 'Bahia', 'State'),  
+('/2/1/2/1/', 'Salvador', 'City'),  
+('/3/1/', 'McMurdo Station', 'City');  
---
+++ sql/hierarchyIdScripts.sql
@@ -0,0 +1,21 @@
+SELECT * FROM [dbo].[SimpleDemo]
+
+SELECT CAST(Level AS nvarchar(100)) AS [Converted Level], *   
+FROM SimpleDemo ORDER BY Level;  
+
+select 
+    *, 
+    level.GetLevel() 
+from
+    dbo.SimpleDemo
+
+-- Everything below Europe
+DECLARE @Europe HIERARCHYID
+SELECT @Europe = Level FROM dbo.SimpleDemo WHERE Location = 'Europe'
+
+SELECT * FROM dbo.SimpleDemo WHERE [Level].IsDescendantOf(@Europe) = 1
+
+DECLARE @Spain HIERARCHYID
+SELECT @Spain = Level FROM dbo.SimpleDemo WHERE Location = 'Spain'
+
+SELECT @Spain.GetDescendant(NULL, NULL)
